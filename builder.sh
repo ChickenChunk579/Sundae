268b017bb8390060e795cce352956095853ef07c
Rhys Tumelty <rhys@tumelty.co.uk>
2026-08-27 17:29:51 +0100

    project clean up

---
+++ builder.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+if [ "$#" -lt 2 ]; then
+    echo "please pass a target, an action, and any optional arguments"
+    echo "usage: $0 {mac} {conf/build} [extra_args...]"
+    exit 1
+fi
+
+
+CMAKE=cmake
+
+# test for cmake
+if command -v "$CMAKE" >/dev/null 2>&1; then
+    echo found cmake
+else
+    echo "please install cmake.
+    exit 1
+fi
+
+
+NINJA=ninja
+
+# test for ninja
+if command -v "$NINJA" >/dev/null 2>&1; then
+    echo found ninja
+else
+    echo "please install ninja.
+    exit 1
+fi
+
+CMAKE_FLAGS="-B build/$1 -S . -GNinja "
+if [[ $1 == "mac" ]]; then
+    CMAKE_FLAGS+="-DPLATFORM=cli -DBACKEND=appkit" 
+else
+    echo unknown platform: $1
+    exit 1
+fi
+
+echo invoking cmake
+$CMAKE $CMAKE_FLAGS
+
+if [[ $2 == "build" || $2 == "run" ]]; then
+    $CMAKE --build build/$1
+    if [[ $2 == "run" ]]; then
+        ./build/$1/butterscotch ${@:3}
+    else
+        echo done
+    fi
+else
+    echo done
+fi
---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+
+  outputs =
+    { self, ... }@inputs:
+    let
+      inherit (inputs.nixpkgs) lib;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forEachSupportedSystem =
+        f:
+        lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShell {
+            name = "sundae-env";
+
+            packages = with pkgs; [
+              clang
+              cmake
+              ninja
+            ];
+
+            shellHook = ''
+              export PS1="\[\e[1;32m\][sundae]\[\e[0m\] \[\e[1;34m\]\u@\h:\w\[\e[0m\]\$ "
+            '';
+          };
+        }
+      );
+    };
+}
